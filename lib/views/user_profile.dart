ecde4a3ad9a16670720ecfc832e55748b968c8b4
Faith Mae Lamberto <filamberto.chmsu@gmail.com>
2025-10-10 22:19:24 +0800

    Clean up: keep only Activity 3, 4, and 5 folders

--- lib/views/user_profile.dart
+++
@@ -1,7 +0,0 @@
-class UserProfile {
-  static String fullName = "Faith Mae I. Lamberto";
-  static String username = "faith_mae";
-  static String address = "Cabay Bulangan, Brgy.5, Silay City";
-  static String contactNumber = "09123456789";
-  static bool deliveryNotif = false;
-}
